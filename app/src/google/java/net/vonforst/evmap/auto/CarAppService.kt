8ea64fffd55d1a5526bfef02d9a9bdf9a2a3cae5
Johan von Forstner <johan.forstner@gmail.com>
2020-12-28 19:12:27 +0100

    add more information to Android Auto app

--- app/src/google/java/net/vonforst/evmap/auto/CarAppService.kt
+++ app/src/google/java/net/vonforst/evmap/auto/CarAppService.kt
@@ -15,12 +15,21 @@ import com.google.android.libraries.car.app.CarContext
 import com.google.android.libraries.car.app.Screen
 import com.google.android.libraries.car.app.model.*
 import com.google.android.libraries.car.app.model.Distance.UNIT_KILOMETERS
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.launch
 import net.vonforst.evmap.R
+import net.vonforst.evmap.api.availability.ChargeLocationStatus
+import net.vonforst.evmap.api.availability.ChargepointStatus
+import net.vonforst.evmap.api.availability.getAvailability
 import net.vonforst.evmap.api.goingelectric.ChargeLocation
 import net.vonforst.evmap.api.goingelectric.GoingElectricApi
+import net.vonforst.evmap.ui.availabilityText
 import net.vonforst.evmap.ui.getMarkerTint
 import net.vonforst.evmap.utils.distanceBetween
+import java.time.Duration
+import java.time.ZonedDateTime
+import kotlin.math.roundToInt
 
 
 class CarAppService : com.google.android.libraries.car.app.CarAppService(), LifecycleObserver {
@@ -88,11 +97,17 @@ class CarAppService : com.google.android.libraries.car.app.CarAppService(), Life
 
 class MapScreen(ctx: CarContext) : Screen(ctx) {
     private var location: Location? = null
+    private var lastUpdateLocation: Location? = null
     private var chargers: List<ChargeLocation>? = null
     private val api by lazy {
         GoingElectricApi.create(ctx.getString(R.string.goingelectric_key), context = ctx)
     }
-    private val distance = 5 // kilometers
+    private val searchRadius = 5 // kilometers
+    private val updateThreshold = 50 // meters
+    private val availabilityUpdateThreshold = Duration.ofMinutes(1)
+    private var availabilities: MutableMap<Long, Pair<ZonedDateTime, ChargeLocationStatus>> =
+        HashMap()
+    private val maxRows = 6
 
     override fun getTemplate(): Template {
         return PlaceListMapTemplate.builder().apply {
@@ -100,11 +115,12 @@ class MapScreen(ctx: CarContext) : Screen(ctx) {
             location?.let {
                 setAnchor(Place.builder(LatLng.create(it)).build())
             }
-            chargers?.take(6)?.let {
+            chargers?.take(maxRows)?.let { chargerList ->
                 val builder = ItemList.Builder()
-                it.forEach { charger ->
+                chargerList.forEach { charger ->
                     builder.addItem(formatCharger(charger))
                 }
+                builder.setNoItemsMessage(carContext.getString(R.string.auto_no_chargers_found))
                 setItemList(builder.build())
             }
             if (chargers == null || location == null) setIsLoading(true)
@@ -128,37 +144,103 @@ class MapScreen(ctx: CarContext) : Screen(ctx) {
 
         return Row.builder().apply {
             setTitle(charger.name)
+            val text = SpannableStringBuilder()
+
+            // distance
             location?.let {
                 val distance = distanceBetween(
                     it.latitude, it.longitude,
                     charger.coordinates.lat, charger.coordinates.lng
                 ) / 1000
-                val distanceSpan = SpannableStringBuilder()
-                    .append(
-                        "distance",
-                        DistanceSpan.create(Distance.create(distance, UNIT_KILOMETERS)),
-                        Spanned.SPAN_INCLUSIVE_INCLUSIVE
-                    )
-                addText(distanceSpan)
+                text.append(
+                    "distance",
+                    DistanceSpan.create(Distance.create(distance, UNIT_KILOMETERS)),
+                    Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
             }
+
+            // power
+            if (text.isNotEmpty()) text.append(" · ")
+            text.append("${charger.maxPower.roundToInt()} kW")
+
+            // availability
+            availabilities[charger.id]?.second?.let { av ->
+                val status = av.status.values.flatten()
+                val available = availabilityText(status)
+                val total = charger.chargepoints.sumBy { it.count }
+
+                if (text.isNotEmpty()) text.append(" · ")
+                text.append(
+                    "$available/$total",
+                    ForegroundCarColorSpan.create(carAvailabilityColor(status)),
+                    Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+            }
+
+            addText(text)
             setMetadata(Metadata.ofPlace(place))
         }.build()
     }
 
     fun updateLocation(location: Location) {
         this.location = location
-        invalidate()
 
-        lifecycleScope.launch {
-            val response = api.getChargepointsRadius(
-                location.latitude,
-                location.longitude,
-                distance,
-                zoom = 16f
-            )
-            chargers =
-                response.body()?.chargelocations?.filterIsInstance(ChargeLocation::class.java)
-            invalidate()
+        if (lastUpdateLocation == null) invalidate()
+
+        if (lastUpdateLocation == null ||
+            location.distanceTo(lastUpdateLocation) > updateThreshold
+        ) {
+            lastUpdateLocation = location
+
+            // update displayed chargers
+            lifecycleScope.launch {
+                // load chargers
+                val response = api.getChargepointsRadius(
+                    location.latitude,
+                    location.longitude,
+                    searchRadius,
+                    zoom = 16f
+                )
+                chargers =
+                    response.body()?.chargelocations?.filterIsInstance(ChargeLocation::class.java)
+
+                // remove outdated availabilities
+                availabilities = availabilities.filter {
+                    Duration.between(
+                        it.value.first,
+                        ZonedDateTime.now()
+                    ) > availabilityUpdateThreshold
+                }.toMutableMap()
+
+                // update availabilities
+                chargers?.take(maxRows)?.map {
+                    lifecycleScope.async {
+                        // update only if not yet stored
+                        if (!availabilities.containsKey(it.id)) {
+                            val date = ZonedDateTime.now()
+                            val availability = getAvailability(it).data
+                            if (availability != null) {
+                                availabilities[it.id] = date to availability
+                            }
+                        }
+                    }
+                }?.awaitAll()
+
+                invalidate()
+            }
         }
+    }
+}
+
+fun carAvailabilityColor(status: List<ChargepointStatus>): CarColor {
+    val unknown = status.any { it == ChargepointStatus.UNKNOWN }
+    val available = status.count { it == ChargepointStatus.AVAILABLE }
+
+    return if (unknown) {
+        CarColor.DEFAULT
+    } else if (available > 0) {
+        CarColor.GREEN
+    } else {
+        CarColor.RED
     }
 }
